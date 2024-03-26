56d71be369f5a06cc85ff2865be7fb2a26b682ab
zSm0k3 <160192976+zSm0k3@users.noreply.github.com>
2024-03-26 20:43:04 -0300

    Add files via upload

---
+++ projetinho_natacao/script.js
@@ -0,0 +1,27 @@
+function bt1(){
+    window.location.href = "socio.html";
+}
+
+function bt2(){
+    window.location.href = "funcionario.html";
+}
+
+function bt3(){
+    window.location.href = "cardapio.html";
+}
+
+function bt4(){
+    window.location.href = "plano.html";
+}
+
+function bt5(){
+    window.location.href = "cadastrar.html";
+}
+
+function btv(){
+    window.location.href = "index.html";
+}
+
+function btl(){
+    window.location.href = "index.html";
+}

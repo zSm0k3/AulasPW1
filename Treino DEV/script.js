a6ddd59c92a50d596f30ff48d58ed84694087d40
zSm0k3 <thexof02@gmail.com>
2024-05-02 22:42:57 -0300

    lepoo

--- Treino DEV/script.js
+++ Treino DEV/script.js
@@ -1,7 +1,15 @@
 var botao = document.getElementById("botao");
+var botao2 = document.getElementById("botao2");
+
 var minhaFuncao = function botao(){
     alert("Eita, você clicou mesmo né");
     window.location.href = URL("https://music.youtube.com/watch?v=7BjtTtS8xCs&list=LM"); 
 };
 
+var minhaFuncao2 = function botao2(){
+    alert("Falei para não clickar!");
+    window.location.href = URL("https://music.youtube.com/watch?v=7BjtTtS8xCs&list=LM"); 
+};
+
 botao.addEventListener("click", minhaFuncao);
+botao2.addEventListener("click", minhaFuncao2);
